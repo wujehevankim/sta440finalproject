a68d42eec8e6765fd3243139c5e714b3cc2f60c4
wujehevankim <evan.kim@duke.edu>
2023-12-12 23:47:21 -0500

    ready to add other predictors to the spy model

--- data/SPY_data_wrangling/SPYdatamodifying2.py
+++ data/SPY_data_wrangling/SPYdatamodifying2.py
@@ -1,22 +1,27 @@
 import pandas as pd
 
-
+# Load the data
 file_path = 'SPY_modified1.csv'  
 data = pd.read_csv(file_path)
 
-
+# Group the data by 'WeekOf'
 grouped = data.groupby('WeekOf')
-filtered_data = grouped.head(1).append(grouped.tail(1))
 
+# Define a custom function to include the first, second to last, and last entry for groups of size 3 or more
+def include_specific_entries(group):
+    if len(group) >= 3:
+        return group.iloc[[0, -2, -1]]  # Include the first, second to last, and last entries
+    return pd.DataFrame()  # Return an empty DataFrame for groups smaller than 3
 
-filtered_data.reset_index(drop=True, inplace=True)
+# Apply the custom function to each group
+filtered_data = grouped.apply(include_specific_entries).reset_index(drop=True)
 
-
+# Convert 'Date' to datetime and sort the data
 filtered_data['Date'] = pd.to_datetime(filtered_data['Date'])
-
-
 filtered_data.sort_values(by='Date', inplace=True)
 
-
+# Save the filtered data to a new CSV file
 filtered_data.to_csv('SPY_modified2.csv', index=False)
 
+
+print(".csv file created")
--- data/SPY_data_wrangling/SPYdatamodifying3.py
+++ data/SPY_data_wrangling/SPYdatamodifying3.py
@@ -9,52 +9,103 @@ data['Date'] = pd.to_datetime(data['Date'])
 
 
 new_df = pd.DataFrame(columns=[
-    'PrecedingWeekToFollowingWeek', 'PrecedingWeekFridayDate', 'PrecedingWeekFridayOpen', 'PrecedingWeekFridayHigh', 
+    'PrecedingWeekToFollowingWeek', 
+
+    'PrecedingWeekThursdayDate', 'PrecedingWeekThursdayOpen', 'PrecedingWeekThursdayHigh', 
+    'PrecedingWeekThursdayLow', 'PrecedingWeekThursdayClose', 'PrecedingWeekThursdayVolume',
+    'PrecedingWeekThursdayDayOfTheWeek', 
+    
+    'PrecedingWeekFridayDate', 'PrecedingWeekFridayOpen', 'PrecedingWeekFridayHigh', 
     'PrecedingWeekFridayLow', 'PrecedingWeekFridayClose', 'PrecedingWeekFridayVolume',
-    'PrecedingWeekFridayDayOfTheWeek', 'FollowingWeekMondayDate', 'FollowingWeekMondayOpen', 'FollowingWeekMondayHigh', 
+    'PrecedingWeekFridayDayOfTheWeek', 
+    
+    
+    'FollowingWeekMondayDate', 'FollowingWeekMondayOpen', 'FollowingWeekMondayHigh', 
     'FollowingWeekMondayLow', 'FollowingWeekMondayClose', 'FollowingWeekMondayVolume', 
-    'FollowingWeekMondayDayOfTheWeek', 'WasMondayOpenHigherThanFridayClose', 'MondayOpenComparedToFridayCloseChangeInPercent','WasMondayCloseHigherThanFridayClose', 'MondayCloseComparedToFridayCloseChangeInPercent'
+    'FollowingWeekMondayDayOfTheWeek',
+    
+    'didFridayCloseHigherThanThursdayClose',
+    'FridayCloseComparedToThursdayCloseChangeInPercent',
+    'wasFridayIntradayPositive',
+    'FridayIntradayChangeInPercent',
+
+
+    'WasMondayOpenHigherThanFridayClose', 
+    'MondayOpenComparedToFridayCloseChangeInPercent',
+    'WasMondayCloseHigherThanFridayClose', 
+    'MondayCloseComparedToFridayCloseChangeInPercent'
 ])
 
 rows = [] 
 
-for i in range(1, len(data), 2):  # Step by 2 as we're looking at pairs of rows
+for i in range(1, len(data)-2, 3):  # Step by 3 as we're looking at length-three's of rows
+    # Assigning values from the 'Thurs' row
+    thursday_row = data.iloc[i]
     # Assigning values from the 'Friday' row
-    friday_row = data.iloc[i]
+    friday_row = data.iloc[i+1]
     # Assigning values from the 'Monday' row of the following week
-    monday_row = data.iloc[i + 1]
+    monday_row = data.iloc[i + 2]
 
 
-    was_monday_open_higher = int(monday_row['Open'] > friday_row['Close'])
-    open_change_percent = ((monday_row['Open'] - friday_row['Close']) / friday_row['Close']) * 100
-    was_monday_close_higher = int(monday_row['Close'] > friday_row['Close'])
-    close_change_percent = ((monday_row['Close'] - friday_row['Close']) / friday_row['Close']) * 100
+    ## ONLY IF IT IS A "NORMAL" WEEK THAT HAD MARKET OPEN MON, THURS, AND FRI, unaffected by "weird" holiday/observation days
+    if (thursday_row['DayOfTheWeek'] == 'Thursday') and (friday_row['DayOfTheWeek'] == 'Friday') and (monday_row['DayOfTheWeek'] == 'Monday'):
+        did_friday_close_higher_than_thursday_close = int(friday_row['Close'] > thursday_row['Close'])
+        friday_close_compared_to_thursday_close_change_in_percent = ((friday_row['Close'] - thursday_row['Close']) / thursday_row['Close']) * 100
+        was_friday_intraday_positive = int(friday_row['Close'] > friday_row['Open'])
+        friday_intraday_change_in_percent = ((friday_row['Close'] - friday_row['Open']) / friday_row['Open']) * 100
+
+        was_monday_open_higher = int(monday_row['Open'] > friday_row['Close'])
+        open_change_percent = ((monday_row['Open'] - friday_row['Close']) / friday_row['Close']) * 100
+        was_monday_close_higher = int(monday_row['Close'] > friday_row['Close'])
+        close_change_percent = ((monday_row['Close'] - friday_row['Close']) / friday_row['Close']) * 100
 
     
-    # Construct the new row for our new DataFrame
-    new_row = {
-        'PrecedingWeekToFollowingWeek': f"{friday_row['WeekOf']} to {monday_row['WeekOf']}",
-        'PrecedingWeekFridayDate': friday_row['Date'].date(),
-        'PrecedingWeekFridayOpen': friday_row['Open'],
-        'PrecedingWeekFridayHigh': friday_row['High'],
-        'PrecedingWeekFridayLow': friday_row['Low'],
-        'PrecedingWeekFridayClose': friday_row['Close'],
-        'PrecedingWeekFridayVolume': friday_row['Volume'],
-        'PrecedingWeekFridayDayOfTheWeek': friday_row['DayOfTheWeek'],
-        'FollowingWeekMondayDate': monday_row['Date'].date(),
-        'FollowingWeekMondayOpen': monday_row['Open'],
-        'FollowingWeekMondayHigh': monday_row['High'],
-        'FollowingWeekMondayLow': monday_row['Low'],
-        'FollowingWeekMondayClose': monday_row['Close'],
-        'FollowingWeekMondayVolume': monday_row['Volume'],
-        'FollowingWeekMondayDayOfTheWeek': monday_row['DayOfTheWeek'],
-        'WasMondayOpenHigherThanFridayClose': was_monday_open_higher,
-        'MondayOpenComparedToFridayCloseChangeInPercent': open_change_percent,
-        'WasMondayCloseHigherThanFridayClose': was_monday_close_higher,
-        'MondayCloseComparedToFridayCloseChangeInPercent': close_change_percent,
-    }
+        # Construct the new row for our new DataFrame
+        new_row = {
+            'PrecedingWeekToFollowingWeek': f"{friday_row['WeekOf']} to {monday_row['WeekOf']}",
 
-    rows.append(new_row)
+            'PrecedingWeekThursdayDate': thursday_row['Date'].date(),
+            'PrecedingWeekThursdayOpen': thursday_row['Open'],
+            'PrecedingWeekThursdayHigh': thursday_row['High'],
+            'PrecedingWeekThursdayLow': thursday_row['Low'],
+            'PrecedingWeekThursdayClose': thursday_row['Close'],
+            'PrecedingWeekThursdayVolume': thursday_row['Volume'],
+            'PrecedingWeekThursdayDayOfTheWeek': thursday_row['DayOfTheWeek'],
+
+            'PrecedingWeekFridayDate': friday_row['Date'].date(),
+            'PrecedingWeekFridayOpen': friday_row['Open'],
+            'PrecedingWeekFridayHigh': friday_row['High'],
+            'PrecedingWeekFridayLow': friday_row['Low'],
+            'PrecedingWeekFridayClose': friday_row['Close'],
+            'PrecedingWeekFridayVolume': friday_row['Volume'],
+            'PrecedingWeekFridayDayOfTheWeek': friday_row['DayOfTheWeek'],
+
+
+            'FollowingWeekMondayDate': monday_row['Date'].date(),
+            'FollowingWeekMondayOpen': monday_row['Open'],
+            'FollowingWeekMondayHigh': monday_row['High'],
+            'FollowingWeekMondayLow': monday_row['Low'],
+            'FollowingWeekMondayClose': monday_row['Close'],
+            'FollowingWeekMondayVolume': monday_row['Volume'],
+            'FollowingWeekMondayDayOfTheWeek': monday_row['DayOfTheWeek'],
+
+            #Friday trends
+            'didFridayCloseHigherThanThursdayClose': did_friday_close_higher_than_thursday_close,
+            'FridayCloseComparedToThursdayCloseChangeInPercent': friday_close_compared_to_thursday_close_change_in_percent,
+            'wasFridayIntradayPositive': was_friday_intraday_positive,
+            'FridayIntradayChangeInPercent': friday_intraday_change_in_percent,
+
+
+            #monday relative to friday trends
+            'WasMondayOpenHigherThanFridayClose': was_monday_open_higher,
+            'MondayOpenComparedToFridayCloseChangeInPercent': open_change_percent,
+            'WasMondayCloseHigherThanFridayClose': was_monday_close_higher,
+            'MondayCloseComparedToFridayCloseChangeInPercent': close_change_percent,
+        }
+
+        rows.append(new_row)
+
+
 
 
 new_df = pd.concat([new_df, pd.DataFrame(rows)], ignore_index=True)
